51923b3fd9c56a831177eafd43a6ffecf3ae636a
yuki_tanaka@shimapri.jp <yuki-tanaka-sp>
2025-06-17 15:52:23 +0900

    0.11.2に変更

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.7.3"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.8.0"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.1.1"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.11.2"),
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.3.2"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.3.2"),
   ],
